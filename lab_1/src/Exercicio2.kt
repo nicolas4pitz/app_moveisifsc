74cd84ce69d4e169243391bdc31bd1b59123e523
nicolas4pitz <nicolas2004pitz@gmail.com>
2026-07-30 20:40:53 -0300

    feat: lab_1 lab_2

--- lab_1/src/Exercicio2.kt
+++ lab_1/src/Exercicio2.kt
@@ -3,28 +3,34 @@ import java.util.Scanner
 fun main(){
 
     print("Nome do aluno: ")
-    val aluno = readln().toInt()
+    val aluno = readln()
 
     print("Nome da disciplina: ")
     val disciplina = readln()
 
-    var numerosDeNotas
+    var numerosDeNotas: Int
     do {
         print("Numero de notas: ")
-        numerosDeNotas = readln()
+        numerosDeNotas = readln().toInt()
         if (numerosDeNotas < 1){
             println("Número de notas inválido")
         }
     } while(numerosDeNotas < 1)
 
-    var media
-    for (i in 1..numerosDeNotas) {
-        print("Nota da : ")
-        val nota = readln()
+    var media: Int = 0
+    var inter: Int = 0
+    do {
+        var leitura = 1
+        print("($leitura) - Nota: ")
+        val nota = readln().toInt()
         if (nota > 10 || nota < 0 ){
-            println("Nota Inválida")
+            println("Nota Inválida, Escreva novamente")
         }else {
+            inter ++
+            leitura ++;
             media += nota
         }
-    }
+    } while (inter != numerosDeNotas )
+    media = media / numerosDeNotas
+    println("Media da disciplina de $disciplina: $media")
 }
